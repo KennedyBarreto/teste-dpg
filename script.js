8308b53d957d8750ad33bf7c575eee6e9c693d5e
Kennedy Barreto <99549329+KennedyBarreto@users.noreply.github.com>
2023-11-08 11:50:46 -0300

    feat: add comentários no código

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
-const shortBtn = document.getElementById('short-btn');
-const reloadBtn = document.getElementById('reload-btn');
+const shortBtn = document.getElementById('short-btn'); // identificação do botão de encurtar
+const reloadBtn = document.getElementById('reload-btn');// identificação do botão de recarregar
 
-shortBtn.addEventListener('click', shortenUrl);
+// captura o envio ao ser apertado o botão de encurtar
+shortBtn.addEventListener('click', shortenUrl); 
 
 function shortenUrl() {
-    var originalUrl = document.getElementById("originalUrl").value;
-    var apiUrl = "https://tinyurl.com/api-create.php?url=" + encodeURIComponent(originalUrl);
+    // armazena o valor da URL original
+    var originalUrl = document.getElementById("originalUrl").value; 
+    // envia o valor da URL original para a API para o processamento e armazena o resultado
+    var apiUrl = "https://tinyurl.com/api-create.php?url=" + encodeURIComponent(originalUrl); 
+    // identificação da area de texto onde o link final será exibido
     shortenedUrlTextarea = document.getElementById("shortenedUrl");
-
+    // exibe a url armazenada na area de texto
     fetch(apiUrl).then(response => response.text()).then(data => {
         shortenedUrlTextarea.value = data;
     }).catch(error => {
+        // caso o valor enviado nao seja uma url valida retorna erro
         shortenedUrlTextarea.value = "Error : Unable to shorten URL!";
     });
 
 }
-
+// adiciona a ação de recarregar a página ao clicar no botão
 reloadBtn.addEventListener('click', () => {
     location.reload();
 });
